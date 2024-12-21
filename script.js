001b785879a9bfed063bcf2b5851d4317b93139d
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2024-12-20 21:00:55 -0500

    Game logic completed.
    Added all functions into playGame function and for each round, the script detects who wins and adds points to appropriate variable. In 5 rounds, whoever has the most points wins or if a tie will say Tie!

--- script.js
+++ script.js
@@ -5,13 +5,100 @@ function getComputerChoice() {
     }
 
     else if (i < (2/3)) {
-        return "paper"
+        return "paper";
     }
 
     else {
-        return "scissors"
+        return "scissors";
     }
 
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    while (true) {
+        s = prompt("Rock, Paper, or Scissors?");
+        choice = s.toLowerCase();
+        if (choice === "rock" || choice === "paper" || choice === "rock") {
+            break;
+        }
+    }
+    return choice;
+}
+
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let round_number = 1;
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            playerResult = "tie";
+        }
+    
+        else if (humanChoice === "rock" && computerChoice === "scissors") {
+            playerResult = "win";
+        }
+    
+        else if (humanChoice === "paper" && computerChoice === "rock") {
+            playerResult = "win";
+        }
+    
+        else if (humanChoice === "scissors" && computerChoice === "paper") {
+            playerResult = "win";
+        }
+    
+        else if (humanChoice === "rock" && computerChoice === "paper") {
+            playerResult = "lose";
+        }
+    
+        else if (humanChoice === "paper" && computerChoice === "scissors") {
+            playerResult = "lose";
+        }
+    
+        else if (humanChoice === "scissors" && computerChoice === "rock") {
+            playerResult = "lose";
+        }
+    
+        if (playerResult === "tie") {
+            console.log(`Tie! Both players picked ${capitalizeFirstLetter(humanChoice)}`);
+        }
+    
+        else if (playerResult === "lose") {
+            console.log(`You Lose! ${capitalizeFirstLetter(computerChoice)} beats ${capitalizeFirstLetter(humanChoice)}.`);
+            computerScore++;
+        }
+    
+        else if (playerResult === "win") {
+            console.log(`You Win! ${capitalizeFirstLetter(humanChoice)} beats ${capitalizeFirstLetter(computerChoice)}.`);
+            humanScore++;
+        }
+    
+    }
+
+    while (round_number <= 5) {
+        let humanSelection = getHumanChoice()
+        let computerSelection = getComputerChoice()
+        playRound(humanSelection, computerSelection);
+        round_number++;
+    }
+    
+    if (humanScore === computerScore) {
+        console.log(`Game is a Tie!`)
+    }
+
+    else if (humanScore > computerScore) {
+        console.log(`Winner is Player!`)
+    }
+
+    else if (computerScore > humanScore) {
+        console.log(`Winner is Computer!`)
+    }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+playGame()
+
