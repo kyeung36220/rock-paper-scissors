d1014af72a593dcd8c3eda972250d1ce3cdd107b
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2024-12-20 20:17:15 -0500

    Adding index skeleton and ComputeChoice function

---
+++ script.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+    let i = Math.random();
+    if (i < (1/3)) {
+        return "rock";
+    }
+
+    else if (i < (2/3)) {
+        return "paper"
+    }
+
+    else {
+        return "scissors"
+    }
+
+}
+
+console.log(getComputerChoice())
