27fdc3c3aec634516ce4a6c6bb8c7b8649c9d614
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2024-12-21 23:27:02 -0500

    Finished Ui with Javascript for RPS

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+let win_amount = 5
+
 function getComputerChoice() {
     let i = Math.random();
     if (i < (1/3)) {
@@ -14,91 +18,84 @@ function getComputerChoice() {
 
 }
 
-function getHumanChoice() {
-    while (true) {
-        s = prompt("Rock, Paper, or Scissors?");
-        choice = s.toLowerCase();
-        if (choice === "rock" || choice === "paper" || choice === "scissors") {
-            break;
-        }
-    }
-    return choice;
-}
-
-
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let round_number = 1;
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            playerResult = "tie";
-        }
-    
-        else if (humanChoice === "rock" && computerChoice === "scissors") {
-            playerResult = "win";
-        }
-    
-        else if (humanChoice === "paper" && computerChoice === "rock") {
-            playerResult = "win";
-        }
-    
-        else if (humanChoice === "scissors" && computerChoice === "paper") {
-            playerResult = "win";
-        }
-    
-        else if (humanChoice === "rock" && computerChoice === "paper") {
-            playerResult = "lose";
-        }
-    
-        else if (humanChoice === "paper" && computerChoice === "scissors") {
-            playerResult = "lose";
-        }
-    
-        else if (humanChoice === "scissors" && computerChoice === "rock") {
-            playerResult = "lose";
-        }
-    
-        if (playerResult === "tie") {
-            console.log(`Tie! Both players picked ${capitalizeFirstLetter(humanChoice)}`);
-        }
-    
-        else if (playerResult === "lose") {
-            console.log(`You Lose! ${capitalizeFirstLetter(computerChoice)} beats ${capitalizeFirstLetter(humanChoice)}.`);
-            computerScore++;
-        }
-    
-        else if (playerResult === "win") {
-            console.log(`You Win! ${capitalizeFirstLetter(humanChoice)} beats ${capitalizeFirstLetter(computerChoice)}.`);
-            humanScore++;
-        }
-    
-    }
-
-    while (round_number <= 5) {
-        let humanSelection = getHumanChoice()
-        let computerSelection = getComputerChoice()
-        playRound(humanSelection, computerSelection);
-        round_number++;
-    }
-    
-    if (humanScore === computerScore) {
-        console.log(`Game is a Tie!`)
-    }
-
-    else if (humanScore > computerScore) {
-        console.log(`Winner is Player!`)
-    }
-
-    else if (computerScore > humanScore) {
-        console.log(`Winner is Computer!`)
-    }
-}
-
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-playGame()
+function playRound(humanChoice, computerChoice) {
+    
+    playerResult = ""
 
+    if (humanChoice === computerChoice) {
+        playerResult = "tie";
+    }
+
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+        playerResult = "win";
+    }
+
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        playerResult = "win";
+    }
+
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        playerResult = "win";
+    }
+
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        playerResult = "lose";
+    }
+
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        playerResult = "lose";
+    }
+
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+        playerResult = "lose";
+    }
+
+    if (playerResult === "tie") {
+        gameText.textContent = `Tie! Both players picked ${capitalizeFirstLetter(humanChoice)}`;
+    }
+
+    else if (playerResult === "lose") {
+        gameText.textContent = `You Lose! ${capitalizeFirstLetter(computerChoice)} beats ${capitalizeFirstLetter(humanChoice)}.`;
+        computerScore++;
+    }
+
+    else if (playerResult === "win") {
+        gameText.textContent = `You Win! ${capitalizeFirstLetter(humanChoice)} beats ${capitalizeFirstLetter(computerChoice)}.`;
+        humanScore++;
+    }
+
+    if (humanScore == win_amount) {
+        gameText.textContent = `Winner is Player!`
+        humanScore = 0
+        computerScore = 0
+    }
+
+    else if (computerScore == win_amount) {
+        gameText.textContent = `Winner is Computer!`
+        humanScore = 0
+        computerScore = 0
+    }
+
+    playerScoreText.textContent = `Player Score: ${humanScore}`
+    computerScoreText.textContent = `Computer Score: ${computerScore}`
+}
+
+const buttons = document.querySelectorAll("button");
+
+function buttonClicked(e) {
+    const humanChoice = e.target.className
+    const computerChoice = getComputerChoice()
+    playRound(humanChoice, computerChoice)
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", buttonClicked);
+});
+
+const gameText = document.querySelector(".gameText")
+const playerScoreText = document.querySelector(".playerScoreText")
+const computerScoreText = document.querySelector(".computerScoreText")
